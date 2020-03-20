bdc5e44ed51741318fb011b0bfaa28109d467c4d
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2020-03-20 11:11:21 +0100

    Add project files.

---
+++ RestItemService/Controllers/ItemsController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.model;
+
+namespace RestItemService.Controllers
+{
+    [Route("api/localItems")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<Item> items = new List<Item>()
+        {
+            new Item(1,"Bread","Low",33),
+            new Item(2,"Bread","Middle",21),
+            new Item(3,"Beer","low",70.5),
+            new Item(4,"Soda","High",21.4),
+            new Item(5,"Milk","Low",55.8)
+        };
+
+        // GET: api/Items
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return items;
+        }
+
+        [HttpGet]
+        [Route("Name/{substring}")]
+        public IEnumerable<Item> GetFromName(string substring)
+        {
+            return items.FindAll(x => x.Name.Contains(substring));
+        }
+
+        [HttpGet]
+        [Route("Quality/{substring}")]
+        public IEnumerable<Item> GetFromQuality(string substring)
+        {
+            return items.FindAll(x => x.Quality.Contains(substring));
+        }
+
+        // GET: api/Items/5
+        [HttpGet]
+        [Route("{id}")]
+        public Item Get(int id)
+        {
+            return items.Find(i => i.Id == id);
+        }
+
+        // POST: api/Items
+        [HttpPost]
+        public void Post([FromBody] Item value)
+        {
+            items.Add(value);
+        }
+
+        // PUT: api/Items/5
+        [HttpPut]
+        [Route("{id}")]
+        public void Put(int id, [FromBody] Item value)
+        {
+            Item item = Get(id);
+            if (item != null)
+            {
+                item.Id = value.Id;
+                item.Name = value.Name;
+                item.Quality = value.Quality;
+                item.Quantity = value.Quantity;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            Item item = Get(id);
+            items.Remove(item);
+        }
+    }
+}
---
+++ RestItemService/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RestItemService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Items API", Version = "v1.0" });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0"));
+
+            app.UseMvc();
+        }
+    }
+}
